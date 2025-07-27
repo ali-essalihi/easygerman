dc3d38fbdc00768e8a33545ca85d1f7dbad4ce85
Mohammed Ali Essalihi <ali.essalihi2004@gmail.com>
2025-07-27 04:32:43 +0100

    Init nextjs app

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Easy German',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
