75de1cb0ef2b6e3a6e825e77900a8a8f9d2a61d0
Mohammed Ali Essalihi <ali.essalihi2004@gmail.com>
2025-07-29 03:32:47 +0100

    Add favicon and Logo

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import Logo from '../components/Logo'
+
 export default function Home() {
-  return <h1 className="text-red-500">Hello, world</h1>
+  return <Logo />
 }
---
+++ apps/web/src/components/Logo.tsx
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <img src="/Logo-120x49.svg" width={120} height={49} alt="Easy German Logo" />
+}
