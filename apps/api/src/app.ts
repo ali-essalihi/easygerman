0653ad1a1058cee7e843a6cc8bd78de8f2318ce4
Mohammed Ali Essalihi <ali.essalihi2004@gmail.com>
2025-07-31 03:30:23 +0100

    Add cors

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,6 +6,8 @@ import authRouter from './routes/auth.routes'
 import hpp from 'hpp'
 import cookieParser from 'cookie-parser'
 import { authenticate } from './middlewares/auth.middlewares'
+import cors from 'cors'
+import env from './env'
 
 const app = express()
 
@@ -14,6 +16,19 @@ app.disable('etag')
 
 app.use(helmet.xContentTypeOptions())
 app.use(helmet.xFrameOptions({ action: 'deny' }))
+
+app.use(
+  cors({
+    methods: ['GET', 'HEAD', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    // Cache results of a preflight request for 1 day (86400 seconds)
+    maxAge: 60 * 60 * 24,
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+    exposedHeaders: [],
+    origin: env.CLIENT_ORIGIN,
+  })
+)
+
 app.use(hpp())
 app.use(cookieParser())
 app.use(express.json())
