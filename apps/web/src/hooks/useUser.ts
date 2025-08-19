e8621470d03020dec535b39cdcef76b1436e0a7d
Mohammed Ali Essalihi <ali.essalihi2004@gmail.com>
2025-08-19 02:50:39 +0100

    Clear swr cache after user logout

--- apps/web/src/hooks/useUser.ts
+++ apps/web/src/hooks/useUser.ts
@@ -1,17 +1,19 @@
 import type { GetCurrentUserRes } from '@easygerman/shared/types'
 import { api } from '@/api'
-import useSWR from 'swr'
+import useSWR, { useSWRConfig } from 'swr'
 import { useCallback } from 'react'
 
 const fetcher = () => api.get<GetCurrentUserRes>('auth/me').json()
 
 export default function useUser() {
   const { data, isLoading, mutate } = useSWR<GetCurrentUserRes | null>('/me', fetcher)
+  const { mutate: globalMutate } = useSWRConfig()
 
   const logout = useCallback(() => {
     if (confirm('Are you sure you want to logout?')) {
       api.post('auth/logout')
       mutate(null, { revalidate: false })
+      globalMutate(() => true, undefined, { revalidate: false })
     }
   }, [])
 
