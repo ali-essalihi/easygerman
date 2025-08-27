af3d16c058056dd9028ae22545f41824f2b35662
Mohammed Ali Essalihi <ali.essalihi2004@gmail.com>
2025-08-27 03:51:41 +0100

    Move revalidate route to api/revalidate/route.ts

--- apps/api/src/services/next-revalidation.ts
+++ apps/api/src/services/next-revalidation.ts
@@ -3,7 +3,7 @@ import env from '../env'
 import { REVALIDATION_SECRET_HEADER, REVALIDATION_TAG_PARAM } from '@easygerman/shared/revalidation'
 
 export function revalidateTags(tags: string[]) {
-  const url = new URL('/revalidate', env.CLIENT_ORIGIN)
+  const url = new URL('/api/revalidate', env.CLIENT_ORIGIN)
   const params = new URLSearchParams()
   tags.forEach((t) => params.append(REVALIDATION_TAG_PARAM, t))
   url.search = params.toString()
